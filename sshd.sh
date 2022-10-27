aeb6fce8a0f6251976972c424053e1a8557069c7
Reem Sadek <reemsadekk@hotmail.com>
2022-10-27 02:31:13 +0200

    Task 03: Securing SSH

---
+++ sshd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter SSH Port Number"
+read SSH_port
+if [SSH_port -lt 65535]
+then
+    if [SSH_port -gt 0]
+    thenTask 03: Securing SSH
+
+        pwd
+    fi
+    pwd
+fi
